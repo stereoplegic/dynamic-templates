d3a8591afcc420c6cf61e429acd2bf6d2a3f3cfb
Joseph Thompson <joe.thompson838@gmail.com>
2020-08-06 07:54:33 -0400

    updated readme file with how to install and command list

--- README.md
+++ README.md
@@ -4,6 +4,24 @@
 
 This CLI is used to generate multiple types of files to make a developers life easier.
 
+## What would you like to see in this package?
+
+If you would like to see different features in this package. Feel free to shoot me an email at `Joseph@josephmt.com`
+
+## How to use
+
+`npm i -g dynamic-templates`
+
+Once installed you have access to the following commands
+
+`dynamic-templates create`
+
+Create will walk you a through a prompt setup to create files from command line. Please see `Working Template Generator` section for options that currently work and those that will be coming soon.
+
+`dynamic-templates version`
+
+`dynamic-templates help`
+
 ## Working Template Generators
 
 ### Functional Component (React)
--- src/lib/utils/versionHandler.js
+++ src/lib/utils/versionHandler.js
@@ -1,11 +1,11 @@
 const chalk = require('chalk')
 const figlet = require('figlet')
 
-const versionHandler = () => {
+const versionHandler = async () => {
 	const packageJSON = require('../../../package.json')
 
-	figlet(
-		`${packageJSON.name}`,
+	await figlet(
+		`dynamic`,
 		{
 			horizontalLayout: 'fitted',
 			verticalLayout: 'fitted',
@@ -17,7 +17,22 @@ const versionHandler = () => {
 				return
 			}
 			console.log(chalk.bold.green(data))
-			console.log(chalk.bold.yellow(`version: ${packageJSON.version}`))
+			figlet(
+				`-templates`,
+				{
+					horizontalLayout: 'fitted',
+					verticalLayout: 'fitted',
+				},
+				(err, data) => {
+					if (err) {
+						console.log('Something went wrong...')
+						console.dir(err)
+						return
+					}
+					console.log(chalk.bold.green(data))
+					console.log(chalk.bold.yellow(`version: ${packageJSON.version}`))
+				},
+			)
 		},
 	)
 }
